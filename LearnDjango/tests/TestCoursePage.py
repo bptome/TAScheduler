6d34b90138b9b7aaf1d8a02c0d3299c58c75e7cd
Omar Hammad <60938024+OmarHHammad@users.noreply.github.com>
2021-12-04 11:51:24 -0600

    Started implementing the create Course page
    This PR is still a work in progress

--- LearnDjango/tests/TestCoursePage.py
+++ LearnDjango/tests/TestCoursePage.py
@@ -7,25 +7,33 @@ from TAInformation.models import Course, User
 class TestCreateCourses(TestCase):
     def setUp(self):
         self.client = Client()
-        self.defaultCourse = Course(none, {"course_name": "english", "instructor_id": 1, "meeting_time": "M-W 1:00 - 2:15",
-                                          "semester": "Spring 2022"})
+        self.defaultCourse = Course(none,
+                                    {"course_name": "english", "instructor_id": 1, "meeting_time": "M-W 1:00 - 2:15",
+                                     "semester": "Spring 2022"})
 
-        self.noNameCourse = Course(none, {"course_name" : none, "instructor_id": 1, "meeting_time":"M-W 1:00 - 2:15", "semester": "Spring 2022"})
+        self.noNameCourse = Course(none, {"course_name": none, "instructor_id": 1, "meeting_time": "M-W 1:00 - 2:15",
+                                          "semester": "Spring 2022"})
         self.user = User(1, "testAdmin", "tA1!", "testAdmin@test.com", "101 W. Wisconsin Ave, Milwaukee, WI 53203",
 
-                       "(414)222-2571")
+                         "(414)222-2571")
+
     def test_default(self):
-        response = self.client.post("/course",  {"course_name": "english", "instructor_id": 1, "meeting_time": "M-W 1:00 - 2:15",
-                                          "semester": "Spring 2022"}, follow=True)
-        self.assertEqual(self.Course.addCourse({"course_name": "english", "instructor_id": 1, "meeting_time": "M-W 1:00 - 2:15",
-                                          "semester": "Spring 2022"}), response)
+        response = self.client.post("/course",
+                                    {"course_name": "english", "instructor_id": 1, "meeting_time": "M-W 1:00 - 2:15",
+                                     "semester": "Spring 2022"}, follow=True)
+        self.assertEqual(
+            self.Course.addCourse({"course_name": "english", "instructor_id": 1, "meeting_time": "M-W 1:00 - 2:15",
+                                   "semester": "Spring 2022"}), response)
 
-    def test_no_role(self):
-        response = self.client.post("/course",  {"course_name" : none, "instructor_id": 1, "meeting_time":"M-W 1:00 - 2:15", "semester": "Spring 2022"}, follow=True)
-        failure_msg = "insufficent permissions to create a course. Please contact your system administrator if you believe this is in error."
-        self.assertEqual(response.context["message"], self.Course.addCourse(), msg=failure_msg)
-
-
+    def test_no_name(self):
+        response = self.client.post("/course",
+                                    {"course_name": none, "instructor_id": 1, "meeting_time": "M-W 1:00 - 2:15",
+                                     "semester": "Spring 2022"}, follow=True)
+        failure_msg = "please enter a name to create a course. Please contact your system administrator if you " \
+                      "believe this is in error. "
+        self.assertEqual(response.context["message"], self.Course.addCourse(
+            {"course_name": none, "instructor_id": 1, "meeting_time": "M-W 1:00 - 2:15",
+             "semester": "Spring 2022"}), msg=failure_msg)
 
     # instructor_id = models.IntegerField()
     # lab = models.CharField(max_length=20)  # TODO: Needs to be an int array!
--- TAInformation/views.py
+++ TAInformation/views.py
@@ -15,27 +15,40 @@ class Home(View):
     def get(self, request):
         return render(request, "login.html", {})
 
+    def post(self, request):
+        return render(request, "createCourse.html",
+                      {"message": "User Created Sucsessfully"})
+class Course(View):
 
-def Course(View):
     def get(self, request):
-        return render(request, "Course.html", {})
+        return render(request, "createCourse.html", {})
 
     def post(self, request):
-        m = User.objects.get(name=request.POST['role'])
-        noPermissions = (m.role != 1)
-        # except:
-        #     noSuchUser = True
-        # if noSuchUser:
-        #     m = User(name=request.POST['name'], password = request.POST['password'])
-        #     m.save()
-        #     request.session["name"] = m.name
-        #     return redirect("/things/")
+        noPermissions = canAccess(User.objects.get('role'), 'admin')
+
         if noPermissions:
-            return render(request, "home.html",
+            return render(request, "createCourse.html",
                           {"message": "insufficent permissions to create a course. Please contact "
                                       "your system administrator if you believe this is in error."})
         else:
-            m = Course(course_name=request.POST['course_name'], )
-            m.save()
-            request.session["name"] = m.name
-            return redirect("/things/")
+            newCourse = addCourse(request.POST['name'], request.POST['instructor'], request.POST['lab'], request.POST['meeting_time'], request.POST['semester'], request.POST['course_type'], request.POST['description'])
+            newCourse.save()
+            request.session["name"] = newCourse.name
+            return redirect("/")
+
+
+#this is a dummy method. will eventually use user_id and return a User() class of the user that matches the user_id
+def findUser(name):
+    pass
+
+#this is a helper method. will eventually use role required & current role to return true if can be accessed, and false if insufficient permissions
+def canAccess(role, required_role):
+    return True
+
+#helper method to take all data from user and return a Course() class instance. This method also generates a user_id for each course
+def addCourse(self, course_name, instructor_name, lab, meeting_time, semester, course_type, description):
+    newCourse = Course(course_name=course_name, instructor_id=findUser(instructor_name), lab=lab,
+                       meeting_time=meeting_time, semester=semester, course_type=course_type, description=description)
+    return newCourse
+
+
