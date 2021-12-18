7580c8e3e4be008d548d9d823da97c4177c73acf
Vaishnavi Manikonda <vaishnavi.manikonda@outlook.com>
2021-12-18 11:43:24 -0600

    implemented logging out and made sure users could only enter pages they had access to, allowed tas and instructors to read public contact information, allowed instructors to look at course assignments

--- TAInformation/Models/ta.py
+++ TAInformation/Models/ta.py
@@ -1,5 +1,7 @@
 from TAInformation.Models.account_type import AccountType
 from TAInformation.Models.base_user import BaseUser
+from TAInformation.Models.validator_methods import *
+from TAInformation.models import Course, User, LabCourseJunction, Lab
 
 
 class TA(BaseUser):
@@ -9,17 +11,49 @@ class TA(BaseUser):
         self.role = AccountType.TA
 
     def display_courses(self):
-        pass
+        #TO DO: MUST BE CHANGED EVENTUALLY, JUST RETURNS ALL COURSES NOW
+        all_courses = Course.objects.all()
+        course_content = []
+        for course in all_courses:
+            all_labs_ids = LabCourseJunction.objects.filter(course_id=course.course_id)
+            labs_string = ""
+            for i in all_labs_ids:
+                labs_string += " " + i.lab_id.lab_name
+            course_information = [
+                course.course_name,
+                course.instructor_id.name,
+                labs_string,
+                course.meeting_time,
+                course.semester,
+                course.course_type,
+                course.description,
+            ]
+            course_content.append(course_information)
+        return course_content
 
+    # precondition: none
+    # post condition: return a String array of all people and their public and private info
     def display_people(self):
-        pass
+        all_users = User.objects.all()
+        print("here")
+        user_content = []
+        for my_user in all_users:
+            user_information = [
+                my_user.name,
+                my_user.password,
+                my_user.email,
+                my_user.home_address,
+                AccountType(my_user.role).__str__(),
+                my_user.phone
+            ]
+            print("here")
+            user_content.append(user_information)
+        return user_content
 
     def display_people_fields(self):
-        pass
+        return ["name", "email", "role", "phone"]
 
     # pre: None
     # post: Returns dict object with message of inability to create accounts
     def create_user(self, new_user):
         return {'result': False, 'message': "Only admins can create new users\n"}
-
-
